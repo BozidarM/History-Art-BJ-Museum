5355aee1c87f9ae166387c8c4c5e463b3f776d88
BozidarM <bozidar.mladenovic1@gmail.com>
2022-01-11 23:06:31 +0100

    aaaaaaa

--- backend-spring/src/main/java/rs/ac/singidunum/backendspring/entity/Comments.java
+++ backend-spring/src/main/java/rs/ac/singidunum/backendspring/entity/Comments.java
@@ -2,11 +2,13 @@ package rs.ac.singidunum.backendspring.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDate;
 
 @Data
+@Document(collection = "comments")
 public class Comments {
     @Id
     private String id;
